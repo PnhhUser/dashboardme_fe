e42b36ce025efd920205546569bafb27c4d263c7
PnhhUser <pnhh240300@gmail.com>
2026-03-21 19:30:20 +0700

    chore: remove environment files from git tracking

--- src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: 'https://dashboardmeapi-production.up.railway.app/api',
-  fileUrl: 'https://dashboardmeapi-production.up.railway.app',
-};
--- src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:5127/api',
-  fileUrl: 'http://localhost:5127',
-};
